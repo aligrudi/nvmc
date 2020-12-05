daa38d74bd4424c1e01d8950878e874608aa03e4
Ali Gholami Rudi <ali@rudi.ir>
2020-12-05 20:49:02 +0330

    ncvnc.cmd: change to script directory before invoking ssh

--- user/ncfwd.cmd
+++ user/ncfwd.cmd
@@ -1,20 +1,27 @@
 echo off
 
-set /p user="User name: "
-set /p addr="VM IP address: "
-set /p lport="Local port (default: 2000): "
-if "%lport%" equ "" set lport="2000"
-set /p rport="VM port (default: 3389): "
-if "%rport%" equ "" set rport="3389"
+rem Default values
+set "user="
+set "addr="
+set "lport=2000"
+set "rport=3389"
+
+rem Read variables
+set /p user="User name (%user%): "
+set /p addr="VM IP address (%addr%): "
+set /p lport="Local port (%lport%)): "
+set /p rport="VM port (%rport%): "
+
+rem Change to script directory
+cd /d "%~dp0"
+
+rem SSH options
+set "opts=-N -L %lport%:%addr%:%rport% %user%@hpc.nit.ac.ir"
 
 echo Using OpenSSH to forward connections...
-ssh -N -L %lport%:%addr%:%rport% %user%@hpc.nit.ac.ir
-
-if "%ERRORLEVEL%" neq "0" (
-	"OpenSSH-Win64\ssh" -N -L %lport%:%addr%:%rport% %user%@hpc.nit.ac.ir
-)
-if "%ERRORLEVEL%" neq "0" (
-	"OpenSSH-Win32\ssh" -N -L %lport%:%addr%:%rport% %user%@hpc.nit.ac.ir
-)
+ssh %opts%
+if "%ERRORLEVEL%" neq "0" "OpenSSH-Win64\ssh" %opts%
+if "%ERRORLEVEL%" neq "0" "OpenSSH-Win32\ssh" %opts%
+if "%ERRORLEVEL%" neq "0" "C:\Program Files\OpenSSH\ssh" %opts%
 if "%ERRORLEVEL%" neq "0" echo OpenSSH failed!
 echo /p ln="Press return to exit..."
--- user/ncvnc.cmd
+++ user/ncvnc.cmd
@@ -1,19 +1,25 @@
 echo off
-set /p vm="VM name: "
 
-set /p port="VNC local port (default: 5909): "
-if "%port%" equ "" set port="5909"
+rem Default values
+set "vm="
+set "port=5909"
+set "user="
 
-set /p user="User name: "
+rem Read variables
+set /p vm="VM name (%vm%): "
+set /p port="VNC local port (%port%): "
+set /p user="User name (%user%): "
+
+rem Change to script directory
+cd /d "%~dp0"
+
+rem SSH options
+set "opts=-L %port%:/home/%user%/%vm%.vnc %user%@hpc.nit.ac.ir ncuser vncs %vm%"
 
 echo Using OpenSSH to forward VNC connections...
-ssh -L %port%:/home/%user%/%vm%.vnc %user%@hpc.nit.ac.ir ncuser vncs %vm%
-
-if "%ERRORLEVEL%" neq "0" (
-	"OpenSSH-Win64\ssh" -L %port%:/home/%user%/%vm%.vnc %user%@hpc.nit.ac.ir ncuser vncs %vm%
-)
-if "%ERRORLEVEL%" neq "0" (
-	"OpenSSH-Win32\ssh" -L %port%:/home/%user%/%vm%.vnc %user%@hpc.nit.ac.ir ncuser vncs %vm%
-)
+ssh %opts%
+if "%ERRORLEVEL%" neq "0" "OpenSSH-Win64\ssh" %opts%
+if "%ERRORLEVEL%" neq "0" "OpenSSH-Win32\ssh" %opts%
+if "%ERRORLEVEL%" neq "0" "C:\Program Files\OpenSSH\ssh" %opts%
 if "%ERRORLEVEL%" neq "0" echo OpenSSH failed!
 echo /p ln="Press return to exit..."
