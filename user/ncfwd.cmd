a592c76242716e56fcb823a456e350b028f00898
Ali Gholami Rudi <ali@rudi.ir>
2021-01-28 17:23:41 +0330

    ncfwd.cmd: merge with ncvnc.cmd

--- user/ncfwd.cmd
+++ user/ncfwd.cmd
@@ -1,23 +1,31 @@
-echo off
+@echo off
 
 rem Default values
 set "host=hpc.nit.ac.ir"
+set "prot=vnc"
 set "user="
+set "vm="
 set "addr="
-set "lport=2000"
-set "rport=3389"
-
-rem Read variables
-set /p user="User name (%user%): "
-set /p addr="VM IP address (%addr%): "
-set /p lport="Local port (%lport%): "
-set /p rport="VM port (%rport%): "
+set "port=5909"
 
 rem Change to script directory
 cd /d "%~dp0"
 
+echo NVMC Client Connection
+echo.
+
+rem Read variables
+set /p host="Server (%host%): "
+set /p prot="Protocol (vnc, rdp, or ssh; default=%prot%): "
+set /p user="User name (%user%): "
+set /p vm="VM name (%vm%): "
+set /p addr="VM IP address (%addr%): "
+set /p port="Local port (%port%): "
+
 rem SSH options
-set "opts=-N -L %lport%:%addr%:%rport% %user%@%host%"
+if "%prot%" equ "vnc" set "opts=-L %port%:/home/%user%/%vm%.vnc %user%@%host% ncuser vncs %vm%"
+if "%prot%" equ "rdp" set "opts=-N -L %port%:%addr%:3389 %user%@%host%"
+if "%prot%" equ "ssh" set "opts=-N -L %port%:%addr%:22 %user%@%host%"
 
 echo Using OpenSSH to forward connections...
 ssh %opts%
